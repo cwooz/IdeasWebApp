eb78a7463d94ef2c74d9289c2eaf598233f428aa
Chris Woosley <woosley.christopher@gmail.com>
2021-02-27 11:25:58 -0500

    basic controller

---
+++ IdeasWebApp/Models/Idea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdeasWebApp.Models
+{
+    public class Idea
+    {
+
+        public int Id { get; set; }
+        public int Title { get; set; }
+        public int Body { get; set; }
+
+        public Idea()
+        {
+
+        }
+    }
+}
